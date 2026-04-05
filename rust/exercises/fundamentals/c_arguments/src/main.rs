f012243ddc4af4f5a48370b95f2e375e62b39728
Aundre Lattie <aundre@gmail.com>
2026-04-05 15:20:26 +0000

    update

--- rust/exercises/fundamentals/c_arguments/src/main.rs
+++ rust/exercises/fundamentals/c_arguments/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    CommandA {
+        #[arg(short, long)]
+        name: String,
+    },
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use clap::Parser;
+
+    #[test]
+    fn test_name() {
+        let args = vec!["my_prog", "command-a", "--name", "John"];
+        let cli = Cli::try_parse_from(args).unwrap();
+
+        match &cli.command {
+            Some(Commands::CommandA { name }) => {
+                println!("name");
+            }
+            None => {}
+        }
+    }
 }
